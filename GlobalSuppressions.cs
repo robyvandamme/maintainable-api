69f423079b3b66acea82d3e13a9bc92b7d13def8
Roby Van Damme <roby@robyvandamme.com>
2021-01-17 09:02:36 +0700

    Configure project namespace and StyleCop analyzers

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "Reviewed")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names must not begin with underscore", Justification = "Reviewed")]
+[assembly: SuppressMessage("Style", "IDE0009:Member access should be qualified.", Justification = "Reviewed")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:PrefixLocalCallsWithThis", Justification = "Reviewed")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1308:VariableNamesMustNotBePrefixed", Justification = "Reviewed.")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1311:StaticReadonlyFieldsMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
